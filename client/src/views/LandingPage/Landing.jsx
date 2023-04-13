f4de55111a4ea97f53dd0e8e850bd50ec8068ff5
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-04-13 16:40:25 -0300

    fix/conflicto Landing

--- client/src/views/LandingPage/Landing.jsx
+++ client/src/views/LandingPage/Landing.jsx
@@ -1,13 +1,13 @@
-import React from "react";
-import PruebaReduxToolKit from "../../components/PruebaReduxToolkit";
-import NavBar from "../../components/NavBar/NavBar";
-import Footer from "../../components/Footer/Footer";
+// import React from "react";
+// import PruebaReduxToolKit from "../../components/PruebaReduxToolkit";
+// import NavBar from "../../components/NavBar/NavBar";
+// import Footer from "../../components/Footer/Footer";
 
 function Landing() {
   return (
     <div>
-      <NavBar />
-      <Footer />
+      {/* <NavBar />
+      <Footer /> */}
     </div>
   );
 }
