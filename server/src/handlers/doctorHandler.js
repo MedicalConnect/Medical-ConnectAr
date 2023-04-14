97853496f9ce86a5a6a39b37cc424eca53ef5e90
Jorge Jimenez <jorge970102@outlook.com>
2023-04-13 23:40:14 -0500

    Se completaron las tareas 05 y 06 Creacion de controllers, handlers y routes para los modelos de Doctor e Historia Clinica

---
+++ server/src/handlers/doctorHandler.js
@@ -0,0 +1,52 @@
+const {createDoctor, getDoctor, getDoctors} = require('../controllers/doctorController')
+
+
+
+const getDoctorHandler = async(req,res) => {
+    const {id} = req.params
+    try {
+        const response = await getDoctor(id)
+        res.status(200).json(response)
+        
+    } catch (error) {
+        res.status(400).json({error: error.message})
+        
+    }
+}
+const getDoctorsHandler = async(req, res) => {
+    const {name} = req.query
+    
+    try {
+        const response = await getDoctors(name)
+        res.status(200).json(response)
+
+    } catch (error) {
+        res.status(400).json({error:error.message})
+        
+    }
+
+}
+
+const createDoctorHandler = async(req, res) => {
+    const {nombre, apellido, fecha_de_nacimiento, sexo, tipo_de_documento, numero_de_documento,pais_de_origen,
+         ciudad, partido, localidad, domicilio, institucion_de_titulacions, fecha_de_titulacion, especilidad,
+         numero_de_matricula, telefono, email, contraseña} = req.body
+    try {
+        const response = await createDoctor(nombre, apellido, fecha_de_nacimiento, sexo, tipo_de_documento, numero_de_documento,pais_de_origen,
+            ciudad, partido, localidad, domicilio, institucion_de_titulacions, fecha_de_titulacion, especilidad,
+            numero_de_matricula, telefono, email, contraseña)
+
+            res.status(200).json(response)
+        
+    } catch (error) {
+        res.status(400).json({error: error.message})
+        
+    }
+
+}
+
+module.exports = {
+    getDoctorsHandler,
+    createDoctorHandler,
+    getDoctorHandler
+}
