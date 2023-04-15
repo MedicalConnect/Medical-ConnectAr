bfc4c7b364a56e00008108bac82819c06f319a26
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-04-15 20:52:51 -0300

    tarea-15-terminado-HistoriaClinica

--- client/src/views/historialclinico/HistorialClinico.jsx
+++ client/src/views/historialclinico/HistorialClinico.jsx
@@ -13,9 +13,9 @@ const HistorialClinico = () => {
                 Navbar
               </a>
             </div>
-            <li className="nav-item dropdown list-style: none">
+            <li className="nav-item dropdown list-style: none btn btn-info">
               <a
-                className="nav-link dropdown-toggle"
+                className="nav-link dropdown-toggle "
                 href=""
                 role="button"
                 data-bs-toggle="dropdown"
@@ -23,7 +23,7 @@ const HistorialClinico = () => {
               >
                 Ingrese Aquí
               </a>
-              <ul className="dropdown-menu">
+              <ul className="dropdown-menu ">
                 <li>
                   <Link className="dropdown-item" to="/ingresopaciente">
                     Ingreso Pacientes
@@ -55,63 +55,85 @@ const HistorialClinico = () => {
           utilidad para que tengas una mejor atención
         </h5>
       </div>
-      <div className="row ">
-        <label for="colFormLabel" className="texto1 col-sm-2 col-form-label">
-          Antecedentes Medicos
-        </label>
-        <div className="col-sm-4">
-          <input
-            type="email"
-            className="form-control"
-            id="colFormLabel"
-            placeholder="Enfermedades en control y tratamiento periodico.."
-          />
-        </div>
-        <label for="colFormLabel" className="col-sm-2 col-form-label">
-          Antecedentes Medicos
-        </label>
-        <div className="col-sm-4">
-          <input
-            type="email"
-            class="form-control"
-            id="colFormLabel"
-            placeholder="Enfermedades en control y tratamiento periodico.."
-          />
-        </div>
-        <label for="colFormLabel" className="col-sm-2 col-form-label">
-          Antecedentes Medicos
-        </label>
-        <div className="col-sm-4">
-          <input
-            type="email"
-            class="form-control"
-            id="colFormLabel"
-            placeholder="Enfermedades en control y tratamiento periodico.."
-          />
-        </div>
-        <label for="colFormLabel" className="col-sm-2 col-form-label">
-          Antecedentes Medicos
-        </label>
-        <div className="col-sm-4">
-          <input
-            type="email"
-            class="form-control"
-            id="colFormLabel"
-            placeholder="Enfermedades en control y tratamiento periodico.."
-          />
-        </div>
-        <label for="colFormLabel" className="col-sm-2 col-form-label">
-          Antecedentes Medicos
-        </label>
-        <div className="col-sm-4">
-          <input
-            type="email"
-            class="form-control"
-            id="colFormLabel"
-            placeholder="Enfermedades en control y tratamiento periodico.."
-          />
-        </div>
+      <div>
+        <img
+          src="https://img.freepik.com/vector-gratis/grupo-personal-medico-que-lleva-iconos-relacionados-salud_53876-43071.jpg"
+          alt="imagen medical connect"
+          className="imageform justify-content-center
+            align-items-center"
+        />
       </div>
+      <form className="formulario ">
+        <div class="row mb-3">
+          <h4 className="h4">Historia Clinica</h4>
+          <label for="colFormLabel" class="col-sm-3 col-form-label">
+            Antecedentes Medicos:{" "}
+          </label>
+          <div class="col-sm-9">
+            <input
+              type="text"
+              class="form-control"
+              id="colFormLabel"
+              placeholder="antecedentes medicos"
+            />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="colFormLabel" class="col-sm-3 col-form-label">
+            Antecedentes Quirurjicos:{" "}
+          </label>
+          <div class="col-sm-9">
+            <input
+              type="text"
+              class="form-control"
+              id="colFormLabel"
+              placeholder="resumen..."
+            />
+          </div>
+        </div>
+        <div class="row mb-3">
+          <label for="colFormLabel" class="col-sm-3 col-form-label">
+            Alergias:{" "}
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="text"
+              class="form-control"
+              id="colFormLabel"
+              placeholder="resumen..."
+            />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label for="colFormLabel" className="col-sm-3 col-form-label">
+            Medicamentos:{" "}
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="text"
+              class="form-control"
+              id="colFormLabel"
+              placeholder="resumen..."
+            />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label for="colFormLabel" className="col-sm-3 col-form-label">
+            Hábitos:{" "}
+          </label>
+          <div className="col-sm-9">
+            <input
+              type="text"
+              class="form-control"
+              id="colFormLabel"
+              placeholder="resumen..."
+            />
+          </div>
+        </div>
+        <button className="boton" type="submit">
+          Enviar
+        </button>
+      </form>
     </div>
   );
 };
