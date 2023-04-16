9f58d8ca4a9b0ee813a49d61a207fdc884364fcd
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-04-15 23:28:10 -0300

    actualizar rama

--- client/src/views/historialclinico/HistorialClinico.jsx
+++ client/src/views/historialclinico/HistorialClinico.jsx
@@ -5,45 +5,16 @@ import "./HistorialClinico.css";
 const HistorialClinico = () => {
   return (
     <div>
-      <div>
-        <div className="container-fluid">
-          <nav className="navbar  navbar-expand-lg navbar-light">
-            <div className="container-fluid">
-              <a className="navbar-brand" href="#">
-                Navbar
-              </a>
-            </div>
-            <li className="nav-item dropdown list-style: none">
-              <a
-                className="nav-link dropdown-toggle"
-                href=""
-                role="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                Ingrese Aquí
-              </a>
-              <ul className="dropdown-menu">
-                <li>
-                  <Link className="dropdown-item" to="/ingresopaciente">
-                    Ingreso Pacientes
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/ingresomedico">
-                    Ingreso Médicos
-                  </Link>
-                </li>
-                <li>
-                  <Link className="dropdown-item" to="/ingresoadmin">
-                    Ingreso Admin
-                  </Link>
-                </li>
-              </ul>
-            </li>
-          </nav>
+      <nav class="navbar  navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src="" alt="logo medical connect" width="30" height="24" />
+          </a>
+          <button type="button" className="btn btn-info">
+            cerrar sesion
+          </button>
         </div>
-      </div>
+      </nav>
 
       <section className="pl-5 bg-gradiente-white-sky-blue">
         <h2>Hola "nombre"</h2>
