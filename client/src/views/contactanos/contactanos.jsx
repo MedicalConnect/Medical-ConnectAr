a2873f2ca966a3b7b55f24d6fd8555e7f2e08f0f
GabRom98 <109051710+GabRom98@users.noreply.github.com>
2023-04-17 01:21:55 -0300

    Se creo un generador de email para la empresa Se añadio 2 links para diferentes numeros de wpp para diferentes encargos, soporte tecnico y atencion al cliente, ademas se pudo usar emailjs para usarlo de generador de mails tanto para empresa y una confirmacion al cliente

--- client/src/views/contactanos/contactanos.jsx
+++ client/src/views/contactanos/contactanos.jsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
 import React from "react";
 import styles from "./contactanos.module.css";
+import emailjs from "@emailjs/browser"
+import {Link} from "react-router-dom"
 
 const Contactanos = () => {
+  const USER_ID= 'G4I9JfqnlE5RMAZOT', 
+  TEMPLATE_ID= 'template_qiagrdj',
+  SERVICE_ID= 'service_1yi776n'
+
+    const sendEmail = (event) => {
+      event.preventDefault();
+   
+      event.preventDefault();
+      const form = event.target;
+      const email = form.elements["user_email"].value;
+      const name = form.elements["user_name"].value;
+      const phone = form.elements["user_phone"].value;
+      const address = form.elements["user_address"].value;
+      const message = form.elements["user_message"].value;
+    
+      if (!name || !email || !message || !phone || !address) {
+        alert("Por favor complete los campos requeridos.");
+        return;
+      }
+    
+      if (!/\S+@\S+\.\S+/.test(email)) {
+        alert("Por favor ingrese un correo electrónico válido.");
+        return;
+      }
+  
+      emailjs.sendForm(SERVICE_ID,TEMPLATE_ID,form,USER_ID)
+      .then(alert("Mensaje Enviado"))
+      .then(response => console.log(response.text))
+      .then(form.reset)
+      .catch(error => console.log(error.text))
+  
+    }
+  
+
+
+
   return (
     <div className={styles.container}>
       <div>
@@ -12,16 +51,20 @@ const Contactanos = () => {
         </h2>
       </div>
       <div className={styles.page}>
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={sendEmail}>
           <h2 className={styles.subtitle}>Contactanos!</h2>
-          <label htmlFor="name">Name:</label>
-          <input type="text" id="name" />
+          <label htmlFor="name">Nombre:</label>
+          <input type="text" id="name" name="user_name"  />
+
           <label htmlFor="email">E-mail:</label>
-          <input type="text" id="email" />
+          <input type="text" id="email" name="user_email" />
+
           <label htmlFor="telefono">Teléfono:</label>
-          <input type="text" id="telefono" />
+          <input type="text" id="telefono" name="user_phone" />
+
           <label htmlFor="domicilio">Domicilio: </label>
-          <input type="text" id="domicilio" />
+          <input type="text" id="domicilio" name="user_address"  />
+          
           <label htmlFor="descripcion">Escribinos:</label>
           <textarea
             className={styles.textDescription}
@@ -29,6 +72,8 @@ const Contactanos = () => {
             placeholder="En que te podemos ayudar?..."
             rows="10"
             cols="30"
+            name="user_message"
+          
           ></textarea>
           <button
             className={styles.enviar}
@@ -59,7 +104,9 @@ const Contactanos = () => {
                   Estamos aquí para ayudarte en lo que necesites. Contactame si
                   tenes alguna pregunta o inquietud.
                 </p>
+                <Link target="_blank" to={"https://api.whatsapp.com/send?phone=541123255776&text=Hola%2C%20Quiero%20contactarme%20con%20Atencion%20al%20Cliente%20de%20Medical%20Connect!"}>
                 <button>Contactar</button>
+                </Link>
               </div>
             </div>
           </div>
@@ -71,7 +118,9 @@ const Contactanos = () => {
                   Detectaste algun error en la pagina? no te preocupes! mandanos
                   un mail y lo resolvemos a la brevedad.
                 </p>
+                <Link target="_blank" to={"https://api.whatsapp.com/send?phone=5492945557886&text=Hola%2C%20Quiero%20contactarme%20con%20soporte%20tecnico%20de%20Medical%20Connect!"}>
                 <button>Contactar</button>
+                </Link>
               </div>
             </div>
           </div>
