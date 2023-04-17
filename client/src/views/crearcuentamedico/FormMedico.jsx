61a20b2102ce5d1c0df729e682fd92231488b42a
JuliPerezApiwan <julieta.perezapiwan@gmail.com>
2023-04-17 03:35:29 -0300

    fix/formulario-doctor

--- client/src/views/crearcuentamedico/FormMedico.jsx
+++ client/src/views/crearcuentamedico/FormMedico.jsx
@@ -390,8 +390,6 @@ const FormMedico = () => {
                          
                           return /^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/.test(value) || 'Error'
                       }
-
-                      
                     }
                   )}
                 />
