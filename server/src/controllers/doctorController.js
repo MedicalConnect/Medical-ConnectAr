214d33ad42dd002fdfbae889bb620a2c1d035574
Alan.-322 <99999496+jon0010@users.noreply.github.com>
2023-04-17 02:39:11 -0300

    fix syntax by prettier

--- server/src/controllers/doctorController.js
+++ server/src/controllers/doctorController.js
@@ -1,95 +1,152 @@
-const {Doctor, Paciente} = require('../db')
-const { Op } = require("sequelize")
+const { Doctor, Paciente } = require("../db");
+const { Op } = require("sequelize");
 
+const createDoctor = async (
+  nombre,
+  apellido,
+  fecha_de_nacimiento,
+  sexo,
+  tipo_de_documento,
+  numero_de_documento,
+  pais_de_origen,
+  provincia,
+  ciudad,
+  nacionalidad,
+  domicilio,
+  titulo,
+  institucion_de_titulacion,
+  fecha_de_titulacion,
+  especilidad,
+  numero_de_matricula,
+  telefono,
+  email,
+  contraseña
+) => {
+  const existingDoc = await Doctor.findOne({
+    where: {
+      numero_de_documento,
+      email,
+    },
+  });
 
-const createDoctor = async(nombre, apellido, fecha_de_nacimiento, sexo, tipo_de_documento, numero_de_documento,pais_de_origen,provincia,
-    ciudad, nacionalidad, domicilio, titulo, institucion_de_titulacion, fecha_de_titulacion, especilidad,
-    numero_de_matricula, telefono, email, contraseña) => {
+  if (existingDoc) throw Error("El doctor ya existe");
+  const doctor = await Doctor.create({
+    nombre,
+    apellido,
+    fecha_de_nacimiento,
+    sexo,
+    tipo_de_documento,
+    numero_de_documento,
+    pais_de_origen,
+    provincia,
+    ciudad,
+    nacionalidad,
+    domicilio,
+    titulo,
+    institucion_de_titulacion,
+    fecha_de_titulacion,
+    especilidad,
+    numero_de_matricula,
+    telefono,
+    email,
+    contraseña,
+  });
 
-        const existingDoc = await Doctor.findOne({
-            where: {
-                numero_de_documento,
-                email,
-                
-            }
-        })
+  return doctor;
+};
 
-        if(existingDoc) throw Error('El doctor ya existe')
-        const doctor = await Doctor.create({nombre, apellido, fecha_de_nacimiento, sexo, tipo_de_documento, numero_de_documento,pais_de_origen,provincia,
-            ciudad, nacionalidad, domicilio, titulo, institucion_de_titulacion, fecha_de_titulacion, especilidad,
-            numero_de_matricula, telefono, email, contraseña})
+const getDoctors = async (name) => {
+  if (!name) {
+    const allDoctors = await Doctor.findAll();
+    return allDoctors;
+  }
 
-        return doctor    
+  const filterDoctors = await Country.findAll({
+    where: { name: { [Op.iLike]: `%${name}%` } },
+  });
 
+  if (!filterDoctors.length) throw Error("No se encontraron resultados");
 
-}
+  return filterDoctors;
+};
 
-const getDoctors = async(name) =>  {
-    if(!name) {
-        const allDoctors = await Doctor.findAll()
-        return allDoctors
+const getDoctor = async (id) => {
+  const doctorFound = await Doctor.findByPk(id, {
+    include: {
+      model: Paciente,
+      through: {
+        attributes: [],
+      },
+    },
+  });
+  if (!doctorFound) throw Error("Doctor no encontrado");
+  else return doctorFound;
+};
+
+const updateDoctor = async (
+  nombre,
+  apellido,
+  fecha_de_nacimiento,
+  sexo,
+  tipo_de_documento,
+  numero_de_documento,
+  pais_de_origen,
+  provincia,
+  ciudad,
+  nacionalidad,
+  domicilio,
+  titulo,
+  institucion_de_titulacion,
+  fecha_de_titulacion,
+  especilidad,
+  numero_de_matricula,
+  telefono,
+  email,
+  contraseña
+) => {
+  await Doctor.update(
+    {
+      nombre,
+      apellido,
+      fecha_de_nacimiento,
+      sexo,
+      tipo_de_documento,
+      numero_de_documento,
+      pais_de_origen,
+      provincia,
+      ciudad,
+      nacionalidad,
+      domicilio,
+      titulo,
+      institucion_de_titulacion,
+      fecha_de_titulacion,
+      especilidad,
+      numero_de_matricula,
+      telefono,
+      email,
+      contraseña,
+    },
+    {
+      where: { nombre: nombre, apellido: apellido, email: email },
     }
+  );
 
-    const filterDoctors = await Country.findAll({
-        where: { name: { [Op.iLike]: `%${name}%` } },
-        })
-    
-    if(!filterDoctors.length) throw Error('No se encontraron resultados')
+  return "Datos actualizados con exito";
+};
 
-    return filterDoctors;
+const addPacienteToDoctor = async (idDoctor, idPaciente) => {
+  const doctor = await Doctor.findOne({
+    where: { id: idDoctor },
+  });
+  if (!doctor) throw Error("Doctor no encontrado");
 
-}
-
-const getDoctor = async(id) => {
-    const doctorFound = await Doctor.findByPk(id, {
-        include: {
-            model: Paciente,
-            through: {
-                attributes: [],
-            }
-        }
-    })
-    if(!doctorFound) throw Error('Doctor no encontrado')
-    else return doctorFound
-
-}
-
-const updateDoctor = async(nombre, apellido, fecha_de_nacimiento, sexo, tipo_de_documento, numero_de_documento,pais_de_origen,provincia,
-    ciudad, nacionalidad, domicilio, titulo, institucion_de_titulacion, fecha_de_titulacion, especilidad,
-    numero_de_matricula, telefono, email, contraseña) => {
-
-        await Doctor.update({nombre, apellido, fecha_de_nacimiento, sexo, tipo_de_documento, numero_de_documento,pais_de_origen,provincia,
-            ciudad, nacionalidad, domicilio, titulo, institucion_de_titulacion, fecha_de_titulacion, especilidad,
-            numero_de_matricula, telefono, email, contraseña}, 
-            {
-                where: {nombre: nombre,
-                    apellido: apellido,
-                    email: email,
-
-            }
-        })
-
-        return 'Datos actualizados con exito'
-
-}
-
-const addPacienteToDoctor = async(idDoctor, idPaciente) =>{
-    const doctor= await Doctor.findOne({
-        where: {id: idDoctor}
-    })
-    if(!doctor) throw Error('Doctor no encontrado')
-
-    doctor.addPaciente(await Paciente.findOne({ where:{id: idPaciente}}))
-    return 'Paciente agregado con exito'
-
-}
+  doctor.addPaciente(await Paciente.findOne({ where: { id: idPaciente } }));
+  return "Paciente agregado con exito";
+};
 
 module.exports = {
-    createDoctor,
-    getDoctors,
-    getDoctor,
-    updateDoctor,
-    
-
-
-}
+  createDoctor,
+  getDoctors,
+  getDoctor,
+  updateDoctor,
+};
