18668115708b57647ab4948cf5ee1677a8ed62b4
Alan.-322 <99999496+jon0010@users.noreply.github.com>
2023-04-24 04:57:42 -0300

    api url is a deploy backend link

---
+++ client/src/helpers/apiUrl.js
@@ -0,0 +1,8 @@
+// const { TYPE_DEPLOY } = process.env;
+
+const apiUrl = "https://medicalconnectapi.onrender.com";
+//   TYPE_DEPLOY === "development"
+// ?
+// : "http://localhost:3001";
+
+export default apiUrl;
