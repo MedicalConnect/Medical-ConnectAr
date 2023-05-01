35c5297b7ee8ba9f4d1cbcc7fa9984bb96554f1a
Alan.-322 <99999496+jon0010@users.noreply.github.com>
2023-05-01 13:49:35 -0300

    remove duplicate code error

--- server/src/handlers/pagos/pagosHandler.js
+++ server/src/handlers/pagos/pagosHandler.js
@@ -1,40 +1,30 @@
 const mercadopago = require("mercadopago");
 require("dotenv").config();
-const { MERCADOPAGO_KEY} = process.env;
+const { MERCADOPAGO_KEY } = process.env;
 
 const pagosHandler = async (req, res) => {
-  const  serv = req.body
+  const serv = req.body;
 
-      let preference = {
-        items: [{
-          id: 1,
-          title: serv.nombre,
-          currency_id: 'ARS',
-          picture_url: serv.imagen,
-          description: serv.detalle,
-          category_id:'art',
-          quantity: 1,
-          unit_price:serv.precio
-
-      }],
-      back_urls: {
-        success:'https://medicalconnectapi.onrender.com/',
-        failure: '',
-        pending:'',
+  let preference = {
+    items: [
+      {
+        id: 1,
+        title: serv.nombre,
+        currency_id: "ARS",
+        picture_url: serv.imagen,
+        description: serv.detalle,
+        category_id: "art",
+        quantity: 1,
+        unit_price: serv.precio,
       },
-      auto_return:'approved',
-      binary_mode: true,
-     
-      }
-
-      mercadopago.configure({
-        access_token: `${MERCADOPAGO_KEY}`
-      })
-      console.log(preference)
-      mercadopago.preferences
-      .create(preference)
-      .then((response) => res.status(200).send(response.body))
-      .catch((err) =>res.status(400).send(console.log(err)))
+    ],
+    back_urls: {
+      success: "https://medicalconnectapi.onrender.com/",
+      failure: "",
+      pending: "",
+    },
+    auto_return: "approved",
+    binary_mode: true,
   };
 
   mercadopago.configure({
