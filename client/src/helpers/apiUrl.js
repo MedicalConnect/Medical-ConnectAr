83794ea0569962982a11e5765578f37fda2fce46
GabRom98 <109051710+GabRom98@users.noreply.github.com>
2023-05-02 08:28:20 -0300

    Se corrigio la ruta general de front end

--- client/src/helpers/apiUrl.js
+++ client/src/helpers/apiUrl.js
@@ -1,6 +1,6 @@
 const isDevelopment = process.env.NODE_ENV === "development";
 const apiUrl = isDevelopment
-  ? "http://localhost:3001/"
-  : "https://medicalconnectapi.onrender.com/";
+  ? "http://localhost:3001"
+  : "https://medicalconnectapi.onrender.com";
   
 export default apiUrl;
