35ed79dabf3ca24d9fe97af99a02e0325ad68c5a
Alan.-322 <88341560+ALANABEL322@users.noreply.github.com>
2023-05-03 20:20:55 -0300

    correcion-db-apiurl

--- client/src/helpers/apiUrl.js
+++ client/src/helpers/apiUrl.js
@@ -2,5 +2,5 @@ const isDevelopment = process.env.NODE_ENV === "development";
 const apiUrl = isDevelopment
   ? "http://localhost:3001"
   : "https://medicalconnectapi.onrender.com";
-  
+
 export default apiUrl;
