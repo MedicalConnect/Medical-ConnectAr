f6759cc7673f04f5fdf55fcd2ed61ad9392d0d0f
Alan.-322 <99999496+jon0010@users.noreply.github.com>
2023-05-03 22:19:45 -0300

    prettier/format-document

--- server/src/db.js
+++ server/src/db.js
@@ -4,15 +4,18 @@ const { Sequelize } = require("sequelize");
 // const paciente = require('./models/Paciente')
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_URL } = process.env;
 
-// const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
-//   logging: false, // set to console.log to see the raw SQL queries
-//   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-// });
+// // const sequelize = new Sequelize(
+// //   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+// //   {
+// //     logging: false, // set to console.log to see the raw SQL queries
+// //     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// //   }
+// );
 
 const sequelize = new Sequelize(DB_URL, {
-  // dialectOptions: {
-  //   ssl: { rejectUnauthorized: false },
-  // },
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
